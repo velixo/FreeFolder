829ea99f9c85a948c4504f66c09c9069b43bd619
Vilhelm <v.lundqvist94@gmail.com>
2014-12-12 01:58:50 +0100

    Added ActionListener to FileAddMenuItem

--- src/gui/menu/FileAddItem.java
+++ src/gui/menu/FileAddItem.java
@@ -4,32 +4,33 @@ import gui.MainWindow;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
 
 import javax.swing.JFileChooser;
 import javax.swing.JMenuItem;
 
 import model.Model;
 
-public class FileAddItem extends JMenuItem implements ActionListener {
+public class FileAddItem extends JMenuItem {
 	private static final long serialVersionUID = -8713598439481016630L;
 	private MainWindow parent;
 	private Model model;
 
-	public FileAddItem(MainWindow parent, Model model) {
+	public FileAddItem(final MainWindow parent, final Model model) {
 		super("Add File...");
 		this.parent = parent;
 		this.model = model;
+		addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO implement
+				System.out.println("FileAddItem.actionPerformed");
+				JFileChooser chooser = new JFileChooser();
+				int returnVal = chooser.showDialog(parent, "Track file");
+				if (returnVal == JFileChooser.APPROVE_OPTION) {
+					model.trackFile(chooser.getSelectedFiles());
+				}
+			}
+		});
 	}
-
-	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO implement
-		JFileChooser chooser = new JFileChooser();
-		int returnVal = chooser.showDialog(parent, "Track file");
-		if (returnVal == JFileChooser.APPROVE_OPTION) {
-			model.trackFile(chooser.getSelectedFiles());
-		}
-	}
-
 }
