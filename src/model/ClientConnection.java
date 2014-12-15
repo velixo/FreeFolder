f291c848b270134bba1d4d2c58c3ceee0f15e6a9
Vilhelm <v.lundqvist94@gmail.com>
2014-12-15 23:38:18 +0100

    Working on adding all the stubs necessary for Server-Client interaction to work

--- src/model/ClientConnection.java
+++ src/model/ClientConnection.java
@@ -1,5 +1,45 @@
 package model;
 
-public class ClientConnection {
+import java.io.File;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.List;
 
+public class ClientConnection {
+	private ObjectOutputStream outputStream = null;
+	private Socket socket;
+	private String ipAddress;
+	private int port = 9876;
+	
+	private int connectionAttempts;
+	private byte[] fileData;
+	private boolean isConnected;
+	
+	public ClientConnection(String ip) {
+		isConnected = false;
+		connectionAttempts = 0;
+	}
+	
+	public void connect() {
+		while(connectionAttempts < 5) {
+			try {
+				socket = new Socket(ipAddress, port);
+				outputStream = new ObjectOutputStream(socket.getOutputStream());
+				isConnected = true;
+			} catch (IOException e) {
+				e.printStackTrace();
+				connectionAttempts++;
+			}
+		}
+		System.out.println("Connection to " + ipAddress + ":" + port + " failed after 5 tries");
+	}
+	
+	public void checkFilesUpdated(List<File> files) {
+		
+	}
+	
+	public void syncFiles(List<File> files) {
+		
+	}
 }
---
+++ src/server/ServerConnection.java
@@ -0,0 +1,40 @@
+package server;
+
+import java.io.File;
+import java.util.List;
+
+public class ServerConnection {
+
+	public void ServerConnection() {
+		
+	}
+	
+	
+	
+	private void checkOldFiles(List<File> files) {
+		
+	}
+	
+	private class ListenForMessagesThread extends Thread {
+		
+		public ListenForMessagesThread() {
+			
+		}
+		
+		@Override
+		public void run() {
+			
+		}
+	}
+	
+	private class WaitForConnectionThread extends Thread {
+		public WaitForConnectionThread() {
+			
+		}
+		
+		@Override
+		public void run() {
+			
+		}
+	}
+}
