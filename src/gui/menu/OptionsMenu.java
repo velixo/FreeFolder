803de72d5a2abeda12d3f2fd5e7fcd4718c03ebc
velixoLaptop <v.lundqvist94@gmail.com>
2015-03-11 00:50:53 +0100

    Cleaned out repo, gonna start fresh

--- src/gui/menu/OptionsMenu.java
+++
@@ -1,7 +0,0 @@
-package gui.menu;
-
-import javax.swing.JMenu;
-
-public class OptionsMenu extends JMenu {
-
-}
--- src/model/Model.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-import java.io.File;
-import java.util.List;
-
-public interface Model {
-	public void trackFiles(File[] file);
-	public List<String> getFileChangeHistory();
-}
