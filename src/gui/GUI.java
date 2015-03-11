010e65a2801f6475a44a750b31a900b2251d6c39
velixoLaptop <v.lundqvist94@gmail.com>
2015-03-11 19:56:46 +0100

    Continued work on the gui

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -1,18 +1,36 @@
 package gui;
 
+import java.awt.Color;
 import java.util.Observable;
 import java.util.Observer;
 
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 
 
-public class GUI implements Observer {
-	private String programTitle = "FreeFolder";
+public class GUI extends JFrame implements Observer {
+	private static final long serialVersionUID = -4343086109680697375L;
+	private final static String programTitle = "FreeFolder";
 	private Observable observable;
 	
 	public GUI(Observable observable) {
+		super(programTitle);
+		observable.addObserver(this);
 		this.observable = observable;
-		JFrame frame = new JFrame(programTitle);
+		JMenuBar menuBar = new JMenuBar();
+		JMenu fileMenu = new JMenu("File");
+		
+		menuBar.add(fileMenu);
+		add(menuBar);
+		
+		BoxLayout fileContainer = new BoxLayout(this, BoxLayout.Y_AXIS);
+		
+		setBackground(Color.WHITE);
+		setSize(800, 400);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 	@Override
