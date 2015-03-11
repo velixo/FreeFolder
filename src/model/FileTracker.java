8a00909113f4af44f75ce02a2e68f01579596902
velixoLaptop <v.lundqvist94@gmail.com>
2015-03-11 13:23:17 +0100

    Added stub classes and test

---
+++ src/model/FileTracker.java
@@ -0,0 +1,9 @@
+package model;
+
+public class FileTracker {
+	private String filePath;
+
+	public FileTracker(String filePath) {
+		
+	}
+}
---
+++ src/model/FileTrackerManager.java
@@ -0,0 +1,21 @@
+package model;
+
+import java.util.List;
+
+public class FileTrackerManager {
+	
+	private List<FileTracker> fileTrackers;
+	
+	public FileTrackerManager() {
+		//TODO implement
+	}
+	
+	public void trackFile(FileTracker ft) {
+		//TODO implement
+	}
+	
+	public List<FileTracker> getTrackedFiles() {
+		//TODO implement
+		return null;
+	}
+}
