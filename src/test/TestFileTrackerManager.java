79e98671097347db2a086ed80ffc98e485d4a0da
velixoLaptop <v.lundqvist94@gmail.com>
2015-03-12 15:32:47 +0100

    Added test for FileTrackerManager.trackFiles

--- src/test/TestFileTrackerManager.java
+++ src/test/TestFileTrackerManager.java
@@ -21,16 +21,30 @@ public class TestFileTrackerManager {
 	}
 	
 	@Test
+	public void testTrackFile() {
+		File f1 = new File("f1");
+		ftManager.trackFile(f1);
+		List<FileTracker> actual = ftManager.getTrackedFiles();
+		
+		List<FileTracker> expected = new ArrayList<FileTracker>();
+		expected.add(new FileTracker(f1));
+
+		assertEquals(expected, actual);
+	}
+	
+	@Test
 	public void testTrackFiles() {
 		File f1 = new File("f1");
 		File f2 = new File("f2");
-		ftManager.trackFile(f1);
-		ftManager.trackFile(f2);
+		File f3 = new File("f3");
+		File[] files = {f1, f2, f3};
+		ftManager.trackFiles(files);
 		List<FileTracker> actual = ftManager.getTrackedFiles();
 		
 		List<FileTracker> expected = new ArrayList<FileTracker>();
 		expected.add(new FileTracker(f1));
 		expected.add(new FileTracker(f2));
+		expected.add(new FileTracker(f3));
 
 		assertEquals(expected, actual);
 	}
