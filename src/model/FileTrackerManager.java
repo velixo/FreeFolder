e38e821707db9a8341ab8a4accfe0e590fa9863d
Vilhelm Lundqvist <v.lundqvist94@gmail.com>
2015-11-05 01:11:12 +0100

    Added stub method for periodically updating checking file status

--- src/model/FileTrackerManager.java
+++ src/model/FileTrackerManager.java
@@ -4,13 +4,17 @@ import java.io.File;
 import java.util.HashSet;
 import java.util.Observable;
 import java.util.Set;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class FileTrackerManager extends Observable{
-	
 	private Set<FileTracker> fileTrackers;
+	private final ScheduledExecutorService scheduler;
 	
 	public FileTrackerManager() {
 		fileTrackers = new HashSet<FileTracker>();
+		scheduler = Executors.newScheduledThreadPool(1);
 	}
 	
 	public void trackFile(File f) {
@@ -29,5 +33,19 @@ public class FileTrackerManager extends Observable{
 	
 	public Set<FileTracker> getTrackedFiles() {
 		return fileTrackers;
+	}
+	
+	/** Starts a thread that periodically checks every file to see if it has been updated recently*/
+	public void setFilestalker(int period, TimeUnit timeUnit) {
+		Runnable filestalkingTask = new Runnable() {
+			public void run() {
+				//for each filetracker
+					//if file has been modified after their lastModified date
+						//mark file as changed - FileTracker.setStatus
+					//else if file missing
+						//mark file as missing
+			}
+		};
+		scheduler.scheduleAtFixedRate(filestalkingTask, 0, period, timeUnit);
 	}
 }
